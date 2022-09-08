f0d45e65f8b8e643b0ae4907d6219d19a42bb546
SirDank <52797753+SirDank@users.noreply.github.com>
2022-09-08 12:09:58 +0530

    fix

--- __src__/dank.tool.py
+++ __src__/dank.tool.py
@@ -5,50 +5,52 @@ from webbrowser import open_new_tab as web
 from dankware import clr_banner, align, cls, clr, magenta, white, chdir
 
 # main
-    
-exec(chdir('exe')); banner='\n     _             _                      _ \n    | |           | |     _              | |\n  _ | | ____ ____ | |  _ | |_  ___   ___ | |\n / || |/ _  |  _ \\| | / )|  _)/ _ \\ / _ \\| |\n( (_| ( ( | | | | | |< ( | |_| |_| | |_| | |\n \\____|\\_||_|_| |_|_| \\_|_)___)___/ \\___/|_|\n                                            \n                    X      \n'
 
-while True: # choose module to execute
-    cls(); print(align(clr_banner(banner).replace('X',f"{white}s i r {magenta}. {white}d a n k {magenta}<3"))) # print randomly coloured and aligned banner
-    modules = [
-        'Software Downloader [UNFINISHED]',
-        'Minecraft Paper Server Builder [UNFINISHED]'
-    ]
-    counter = 1; to_print = ""
-    for module in modules: to_print += f"\n\n    {counter} > {module}"; counter += 1
-    choice = input(clr(f"  Modules: {to_print}\n\n  Choice: "))
-    if choice.isdigit() and int(choice) > 0 and int(choice) < int(len(modules))+1: break
-
-choice = modules[int(choice)-1]
-
-try:
-    if choice == "Software Downloader": project = "dank.downloader"
-    elif choice == "Minecraft Paper Server Builder": project = "dank.server-builder"
-    # elif choice == "Instagram Ghostbuster": project = "dank.insta-tool"
-    # elif choice == "Chatbot": project = "dank.ai"
-    # elif choice == "Analyze suspicious file":
-    # elif choice == "Sussy Optimiser":
-    # elif choice == "HWID Spoofer":
-    # elif choice == "Discord Backup":
-    # elif choice == "Spotify Ad Blocker":
-    # elif choice == "Spotify Downloader":
-    # elif choice == "Youtube Music Downloader":
-    # elif choice == "Youtube Video Downloader":
-    # elif choice == "Temp File Cleaner":
+while True:
     
-    while True: # get src from github
-        try: code = requests.get(f"https://raw.githubusercontent.com/SirDank/dank.tool/main/__modules__/{project}.py").content.decode(); break
-        except: wait = input(clr(f"\n  > Failed to get src for {project}! Make sure you are connected to the Internet! Press [ENTER] to try again... ",2))
-    
-    if code == "404: Not Found": print(clr(f"  > {project} has not yet been released! Returning to menu in 5s...",2)); time.sleep(5)
-    else: exec(code); cls(); print(clr(f"\n  > {project} executed successfully! Returning to menu in 5s...")); time.sleep(5)
+    exec(chdir('exe')); banner='\n     _             _                      _ \n    | |           | |     _              | |\n  _ | | ____ ____ | |  _ | |_  ___   ___ | |\n / || |/ _  |  _ \\| | / )|  _)/ _ \\ / _ \\| |\n( (_| ( ( | | | | | |< ( | |_| |_| | |_| | |\n \\____|\\_||_|_| |_|_| \\_|_)___)___/ \\___/|_|\n                                            \n                    X      \n'
 
-except Exception as exp:
-    
-    cls(); exc_type, exc_obj, exc_tb = sys.exc_info()
-    print(clr(f"\n  > Error: {str(exp)} | {exc_type} | Line: {exc_tb.tb_lineno}",2))
-    print(clr(f"\n  > Please take a screenshot of this and post it on > https://github.com/SirDank/dank.tool/issues/new"))
-    print(clr("\n  > Opening in 3s..."))
-    time.sleep(3); web("https://github.com/SirDank/dank.tool/issues/new")
-    wait = input(clr(f"\n  > Press [ENTER] to continue: "))
+    while True: # choose module to execute
+        cls(); print(align(clr_banner(banner).replace('X',f"{white}s i r {magenta}. {white}d a n k {magenta}<3"))) # print randomly coloured and aligned banner
+        modules = [
+            'Software Downloader [UNFINISHED]',
+            'Minecraft Paper Server Builder [UNFINISHED]'
+        ]
+        counter = 1; to_print = ""
+        for module in modules: to_print += f"\n\n    {counter} > {module}"; counter += 1
+        choice = input(clr(f"  Modules: {to_print}\n\n  Choice: "))
+        if choice.isdigit() and int(choice) > 0 and int(choice) < int(len(modules))+1: break
+
+    choice = modules[int(choice)-1]
+
+    try:
+        if choice == "Software Downloader": project = "dank.downloader"
+        elif choice == "Minecraft Paper Server Builder": project = "dank.server-builder"
+        # elif choice == "Instagram Ghostbuster": project = "dank.insta-tool"
+        # elif choice == "Chatbot": project = "dank.ai"
+        # elif choice == "Analyze suspicious file":
+        # elif choice == "Sussy Optimiser":
+        # elif choice == "HWID Spoofer":
+        # elif choice == "Discord Backup":
+        # elif choice == "Spotify Ad Blocker":
+        # elif choice == "Spotify Downloader":
+        # elif choice == "Youtube Music Downloader":
+        # elif choice == "Youtube Video Downloader":
+        # elif choice == "Temp File Cleaner":
+        
+        while True: # get src from github
+            try: code = requests.get(f"https://raw.githubusercontent.com/SirDank/dank.tool/main/__modules__/{project}.py").content.decode(); break
+            except: wait = input(clr(f"\n  > Failed to get src for {project}! Make sure you are connected to the Internet! Press [ENTER] to try again... ",2))
+        
+        if code == "404: Not Found": print(clr(f"\n  > {project} has not yet been released! Returning to menu in 5s...",2)); time.sleep(5)
+        else: exec(code); cls(); print(clr(f"\n  > {project} executed successfully! Returning to menu in 5s...")); time.sleep(5)
+
+    except Exception as exp:
+        
+        cls(); exc_type, exc_obj, exc_tb = sys.exc_info()
+        print(clr(f"\n  > Error: {str(exp)} | {exc_type} | Line: {exc_tb.tb_lineno}",2))
+        print(clr(f"\n  > Please take a screenshot of this and post it on > https://github.com/SirDank/dank.tool/issues/new"))
+        print(clr("\n  > Opening in 3s..."))
+        time.sleep(3); web("https://github.com/SirDank/dank.tool/issues/new")
+        wait = input(clr(f"\n  > Press [ENTER] to continue: "))
 
