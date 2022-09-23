b847d78a074e828c9ddd0475a3ce620e1c8ca5eb
SirDank <52797753+SirDank@users.noreply.github.com>
2022-09-24 01:58:35 +0530

    beta release 1 fix
    Former-commit-id: 4b37cc84e074991ca035758753bc0dac3bbd80b0

--- __src__/executor.py
+++ __src__/executor.py
@@ -19,16 +19,15 @@ from packaging.version import parse
 
 # change directory to exe's location
 
-project = "dank.tool"
 current_version = "0.2"
-title(f"{project} [ initializing ]"); exec(chdir('exe'))
+title("dank.tool [ initializing ]"); exec(chdir('exe'))
 print(clr(f"\n  > Version: {current_version}"))
 
 # get latest version
 
 while True:
     try:
-        latest_version = requests.get(f"https://raw.githubusercontent.com/SirDank/{project}/main/__src__/executor_version.txt").content.decode()
+        latest_version = requests.get("https://raw.githubusercontent.com/SirDank/dank.tool/main/__src__/executor_version.txt").content.decode()
         if "Not Found" in latest_version: latest_version = 0
         #else: latest_version = float(latest_version); break
         else: break
@@ -38,12 +37,12 @@ while True:
 
 def download_latest():
 
-    print(clr(f"\n  > Downloading {project}-latest.exe..."))
+    print(clr("\n  > Downloading dank.tool-latest.exe..."))
     while True:
-        try: data = requests.get(f"https://github.com/SirDank/{project}/blob/main/{project}.exe?raw=true", allow_redirects=True).content; break
+        try: data = requests.get("https://github.com/SirDank/dank.tool/blob/main/dank.tool.exe?raw=true", allow_redirects=True).content; break
         except: wait = input(clr("\n  > Failed to download! Make sure you are connected to the Internet! Press [ENTER] to try again... ",2))
-    open(f"{project}-latest.exe","wb+").write(data); data = None
-    open("dankware-updater.cmd","w+").write(f"@echo off\ntitle dankware-updater\ncolor 0d\ntimeout 3\ndel /F {project}.exe\nren {project}-latest.exe {project}.exe\nstart {project}.exe\ndel \"%~f0\" >nul 2>&1")
+    open("dank.tool-latest.exe","wb+").write(data); data = None
+    open("dankware-updater.cmd","w+").write("@echo off\ntitle dankware-updater\ncolor 0d\ntimeout 3\ndel /F dank.tool.exe\nren dank.tool-latest.exe dank.tool.exe\nstart dank.tool.exe\ndel \"%~f0\" >nul 2>&1")
     print(clr("\n  > Downloaded!\n\n  > Starting in 3s..."))
     toast = ToastNotifier(); toast.show_toast("dank.tool", "Updating and restarting in 5s...", duration = 3, icon_path = "dankware.ico", threaded = False)
     time.sleep(3); os.system(f"start dankware-updater.cmd"); sys.exit()
@@ -52,26 +51,26 @@ if parse(latest_version) > parse(current_version):
     choice = input(clr(f"\n  > Update Found: {latest_version}\n\n  > Download latest version? [ y / n ]: ")).lower()
     if choice == "y": download_latest()
 elif latest_version == current_version: print(clr(f"\n  > Latest Version!"))
-else: print(clr(f"\n  > Development Version!"))
+else: print(clr("\n  > Development Version!"))
 
 # get main src from github
 
 while True:
-    try: code = requests.get(f"https://raw.githubusercontent.com/SirDank/{project}/main/__src__/{project}.py").content.decode(); break
+    try: code = requests.get("https://raw.githubusercontent.com/SirDank/dank.tool/main/__src__/dank.tool.py").content.decode(); break
     except: wait = input(clr("\n  > Failed to get src! Make sure you are connected to the Internet! Press [ENTER] to try again... ",2))
 
 # execute, catch errors if any
 
-time.sleep(3); title(project); cls()
+time.sleep(3); title("dank.tool"); cls()
 
 try: exec(code)
 except Exception as exp:
 
     cls(); exc_type, exc_obj, exc_tb = sys.exc_info()
     print(clr(f"\n  > Error: {str(exp)} | {exc_type} | Line: {exc_tb.tb_lineno}",2))
-    print(clr(f"\n  > Please take a screenshot of this and post it on https://github.com/SirDank/{project}/issues/new"))
+    print(clr("\n  > Please take a screenshot of this and post it on https://github.com/SirDank/dank.tool/issues/new"))
     print(clr("\n  > Opening in 3s..."))
-    time.sleep(3); web(f"https://github.com/SirDank/{project}/issues/new")
+    time.sleep(3); web("https://github.com/SirDank/dank.tool/issues/new")
 
     if not latest_version == current_version:
         choice = input(clr("\n  > Download latest version? [ y / n ]: ")).lower()
