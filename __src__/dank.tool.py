336699bc50e0d53bde6a64aaac233f2334d8b550
SirDank <52797753+SirDank@users.noreply.github.com>
2022-10-06 23:48:08 +0530

    new debug function
    code cleanup
    
    
    Former-commit-id: 4bb38a896719904da4633fcc6f18ad4ba6b3712e

--- __src__/dank.tool.py
+++ __src__/dank.tool.py
@@ -1,7 +1,7 @@
+import os
 import sys
 import time
 import requests
-from webbrowser import open_new_tab as web
 from dankware import clr_banner, align, cls, clr, magenta, white, chdir, title
 
 # main
@@ -15,27 +15,34 @@ while True:
         modules = [
             'Minecraft Server Builder',
             'Minecraft Server Scanner',
+            'Spotify Ad Blocker',
             'Software Downloader [UNFINISHED]',
         ]
         counter = 1; to_print = ""
         for module in modules: to_print += f"\n\n    {counter} > {module}"; counter += 1
         choice = input(clr(f"\n  - Modules: {to_print}\n\n  - Choice: ") + magenta)
-        if choice.isdigit() and int(choice) > 0 and int(choice) < int(len(modules))+1: break
-
-    choice = modules[int(choice)-1]
+        if choice.isdigit() and int(choice) > 0 and int(choice) < int(len(modules))+1:
+            choice = modules[int(choice)-1]; break
+        elif choice == 'debug':
+            cls()
+            while True:
+                cmd = input(clr("\n  > ") + white)
+                if cmd == 'exit': sys.exit()
+                try: exec(cmd)
+                except Exception as exc: print(clr(f"\n  > ERROR: {exc}",2))
 
     try:
         if "Minecraft Server Builder" in choice: project = "dank.minecraft-server-builder"
         elif "Minecraft Server Scanner" in choice: project = "dank.minecraft-server-scanner"
         elif "Software Downloader" in choice: project = "dank.downloader"
+        elif "Spotify Ad Blocker" in choice: project = "dank.spotx-win"
+        # elif "Spotify Downloader" in choice:
         # elif "Instagram Ghostbuster" in choice: project = "dank.insta-tool"
         # elif "Chatbot" in choice: project = "dank.ai"
         # elif "Analyze suspicious file" in choice:
         # elif "Sussy Optimiser" in choice:
         # elif "HWID Spoofer" in choice:
         # elif "Discord Backup" in choice:
-        # elif "Spotify Ad Blocker" in choice:
-        # elif "Spotify Downloader" in choice:
         # elif "Youtube Video Downloader" in choice:
         # elif "Temp File Cleaner" in choice:
         else: project = "404"
@@ -44,7 +51,7 @@ while True:
             try: code = requests.get(f"https://raw.githubusercontent.com/SirDank/dank.tool/main/__modules__/{project}.py").content.decode(); break
             except: wait = input(clr(f"\n  > Failed to get src for {project}! Make sure you are connected to the Internet! Press [ENTER] to try again... ",2))
         
-        if code == "404: Not Found": print(clr(f"\n  > {project} has not yet been released! Returning to menu in 5s...",2)); time.sleep(5)
+        if code == "404: Not Found": print(clr(f"\n  > {project} has not been released yet! Returning to menu in 5s...",2)); time.sleep(5)
         else:
             cls(); exec(code.replace("exec_mode = 'script'", "exec_mode = 'exe'").replace('exec_mode = "script"', 'exec_mode = "exe"'))
             cls(); print(clr(f"\n  > {project} executed successfully! Returning to menu in 5s...")); time.sleep(5)
@@ -55,6 +62,8 @@ while True:
         print(clr(f"\n  > Error: {str(exp)} | {exc_type} | Line: {exc_tb.tb_lineno}",2))
         print(clr(f"\n  > Please take a screenshot of this and post it on > https://github.com/SirDank/dank.tool/issues/new"))
         print(clr("\n  > Opening in 3s..."))
-        time.sleep(3); web("https://github.com/SirDank/dank.tool/issues/new")
+        time.sleep(3); url = "https://github.com/SirDank/dank.tool/issues/new"
+        if os.name == 'nt': os.system(f"start {url}")
+        else: os.system(f"xdg-open {url}")
         wait = input(clr(f"\n  > Press [ENTER] to continue: "))
 
