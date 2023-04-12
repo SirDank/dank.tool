55bd770e26f4b94426597f9be9ab67ac6e237ec6
SirDank <52797753+SirDank@users.noreply.github.com>
2023-04-12 21:31:38 +0530

    Update dank.fusion-fall.py

--- __modules__/dank.fusion-fall.py
+++ __modules__/dank.fusion-fall.py
@@ -276,8 +276,8 @@ def main():
     cab_name = str(cab_path.split('\\')[-1])
     print(clr(logger(f"  > tabledata = Asset.from_file(open('{cab_path}', 'rb'))")))
     tabledata = Asset.from_file(open(cab_path, 'rb'))
-    if input(clr("  > Print Available TableData Objects? [y/n]: ")).lower() == 'y':
-        print(clr(logger(f"  > Available TableData Objects: {' '.join(tabledata.objects.keys())}")))
+    #if input(clr("  > Print Available TableData Objects? [y/n]: ")).lower() == 'y':
+    #    print(clr(logger(f"  > Available TableData Objects: {' '.join(tabledata.objects.keys())}")))
     index = int(input(clr('  > TableData Object Index: ') + green))
     print(clr(logger(f"  > xdtdata = tabledata.objects[{index}].contents")))
     xdtdata = tabledata.objects[index].contents
