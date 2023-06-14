24f5a6faf1d9da6302e089c1dbf5ee6563ee306c
SirDank <52797753+SirDank@users.noreply.github.com>
2023-06-14 22:00:36 +0530

    Update dank.spotify.py
    fix
    
    
    Former-commit-id: 01251b971a408e7cf56585ff925720af52abaff9

--- __modules__/dank.spotify.py
+++ __modules__/dank.spotify.py
@@ -14,7 +14,7 @@ def main():
         cls()
         try:
             os.system('powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; iex ((New-Object System.Net.WebClient).DownloadString(\'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1\'))} -confirm_uninstall_ms_spoti -confirm_spoti_recomended_over -podcasts_off -cache_on -block_update_on -start_spoti -new_theme -adsections_off -lyrics_stat spotify; Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex"')
-            os.system('start cmd.exe @cmd /k "spicetify restore backup apply"')
+            os.system('start cmd.exe @cmd /k "spicetify backup apply"') # restore
             break
         except: input(clr(f"\n  > Failed to launch! Make sure you are connected to the internet! Press [ENTER] to try again... ",2))
     time.sleep(5)
