ab8f42c28553372ec2db0a7d290f72a59a886480
SirDank <52797753+SirDank@users.noreply.github.com>
2023-06-18 00:08:13 +0530

    dank.win-activate

--- __modules__/dank.spotify.py
+++ __modules__/dank.spotify.py
@@ -10,7 +10,7 @@ def main():
     banner2 = '\n\nMP""""""`MM          oo                     dP   oo .8888b          \nM  mmmmm..M                                 88      88   "          \nM.      `YM 88d888b. dP .d8888b. .d8888b. d8888P dP 88aaa  dP    dP \nMMMMMMM.  M 88\'  `88 88 88\'  `"" 88ooood8   88   88 88     88    88 \nM. .MMM\'  M 88.  .88 88 88.  ... 88.  ...   88   88 88     88.  .88 \nMb.     .dM 88Y888P\' dP `88888P\' `88888P\'   dP   dP dP     `8888P88 \nMMMMMMMMMMM 88                                                  .88 \n            dP                                              d8888P  \n'
     
     cls(); print(align(clr(banner1,4,colours=[white, white_normal, red, red_normal, red_dim]) + align(clr('+')) + align(clr(banner2,4,colours=[white, white_normal, red, red_normal, red_dim]))))
-    input(clr("\n  > Hit [ ENTER ] to begin installation..."))
+    input(clr("\n  > Hit [ ENTER ] to begin installation... "))
     while True:
         cls()
         try:
