9a4841af45f8e566235c29f3994f14501cb101fe
SirDank <52797753+SirDank@users.noreply.github.com>
2023-06-29 09:26:29 +0530

    Update dank.spotify.py
    Former-commit-id: 134283180e11256ca1f50c95465d1d888eee2897

--- __modules__/dank.spotify.py
+++ __modules__/dank.spotify.py
@@ -1,7 +1,7 @@
 import os
 import time
 from dankware import title, cls, clr, align
-from dankware import white, white_normal, green, green_normal, green_dim
+from dankware import white, white_normal, green, green_normal, green_dim, yellow, yellow_normal, yellow_dim
 
 def main():
     
@@ -9,7 +9,7 @@ def main():
     banner1 = '\n\n\n .oooooo..o          amd64fox\'s      .   ooooooo  ooooo \nd8P\'    `Y8                        .o8    `8888    d8\'  \nY88bo.      oo.ooooo.   .ooooo.  .o888oo    Y888..8P    \n `"Y8888o.   888\' `88b d88\' `88b   888       `8888\'     \n     `"Y88b  888   888 888   888   888      .8PY888.    \noo     .d8P  888   888 888   888   888 .   d8\'  `888b   \n8""88888P\'   888bod8P\' `Y8bod8P\'   "888" o888o  o88888o \n             888                                        \n            o888o                                       \n                                                        \n'
     banner2 = '\n\nMP""""""`MM          oo                     dP   oo .8888b          \nM  mmmmm..M                                 88      88   "          \nM.      `YM 88d888b. dP .d8888b. .d8888b. d8888P dP 88aaa  dP    dP \nMMMMMMM.  M 88\'  `88 88 88\'  `"" 88ooood8   88   88 88     88    88 \nM. .MMM\'  M 88.  .88 88 88.  ... 88.  ...   88   88 88     88.  .88 \nMb.     .dM 88Y888P\' dP `88888P\' `88888P\'   dP   dP dP     `8888P88 \nMMMMMMMMMMM 88                                                  .88 \n            dP                                              d8888P  \n'
     
-    cls(); print(align(clr(banner1,4,colours=[white, white_normal, green, green_normal, green_dim]) + align(clr('+')) + align(clr(banner2,4,colours=[white, white_normal, green, green_normal, green_dim]))))
+    cls(); print(align(clr(banner1,4,colours=[white, white_normal, green, green_normal, green_dim]) + align(clr('+')) + align(clr(banner2,4,colours=[white, white_normal, yellow, yellow_normal, yellow_dim]))))
     input(clr("\n  > Hit [ ENTER ] to begin installation... "))
     while True:
         cls()
