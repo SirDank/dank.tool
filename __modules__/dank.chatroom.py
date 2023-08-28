9b2ec72900ad173a2d6c52364b374516844b73ec
SirDank <52797753+SirDank@users.noreply.github.com>
2023-08-28 10:35:18 +0530

    Update dank.chatroom.py

--- __modules__/dank.chatroom.py
+++ __modules__/dank.chatroom.py
@@ -110,7 +110,7 @@ def chatroom_output():
                         icon = {'src': f'{os.path.dirname(__file__)}\\dankware.ico', 'placement': 'appLogoOverride'} if os.path.exists(f'{os.path.dirname(__file__)}\\dankware.ico') else None,
                     )
 
-                if chat[_].startswith("[dank.server]"):
+                if chat[_].startswith("[dank.server]") or chat[_].startswith("[MC-Server"):
                     chat[_] = clr(chat[_], colour_two=green)
                 elif chat[_].startswith("[SirDank]"):
                     chat[_] = clr(chat[_].replace("[SirDank]",f"[{green}SirDank{red}]"))
