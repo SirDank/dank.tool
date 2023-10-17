2238e693da71f3cd99ba83f0963884d8693089d9
SirDank <52797753+SirDank@users.noreply.github.com>
2023-10-18 01:58:49 +0530

    .

--- __src__/executor.py
+++ __src__/executor.py
@@ -47,19 +47,8 @@ from unitypackff.modding import import_texture, import_mesh, import_audio
 
 import numpy
 from ursina import *
-from ursina.shaders.unlit_shader import unlit_shader
-from ursina.shaders.lit_with_shadows_shader import lit_with_shadows_shader
-from ursina.shaders.normals_shader import normals_shader
-from ursina.shaders.basic_lighting_shader import basic_lighting_shader
-from ursina.shaders.triplanar_shader import triplanar_shader
-from ursina.shaders.matcap_shader import matcap_shader
-from ursina.shaders.colored_lights_shader import colored_lights_shader
-from ursina.shaders.transition_shader import transition_shader
-from ursina.shaders.screenspace_shaders.camera_vertical_blur import camera_vertical_blur_shader
-from ursina.shaders.screenspace_shaders.camera_contrast import camera_contrast_shader
-from ursina.shaders.screenspace_shaders.camera_grayscale import camera_grayscale_shader
-from ursina.shaders.screenspace_shaders.fxaa import fxaa_shader
-from ursina.shaders.screenspace_shaders.ssao import ssao_shader
+from ursina.shaders import *
+from ursina.shaders import texture_blend_shader
 from ursina.prefabs.first_person_controller import FirstPersonController
 
 # required packages for executor.py
