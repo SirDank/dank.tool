e9bb4acce6236f2ab732d9314d7fcd593e7eb731
SirDank <52797753+SirDank@users.noreply.github.com>
2023-11-02 19:56:26 +0530

    Update updater.py
    Former-commit-id: bc33b0d82fd29cd02e3328714a68dc33a8876eb9

--- __src__/updater.py
+++ __src__/updater.py
@@ -12,7 +12,7 @@ except: exec("DANK_TOOL_VERSION = current_version") # current_version defined in
 
 try:
     response = session.get("https://api.github.com/repos/SirDank/dank.tool/releases", headers = {"User-Agent": "dank.tool", "Accept": "application/vnd.github.v3+json"})
-                                                                # REMOVE THE BELOW CHECK IN THE FAR FUTURE!
+    # REMOVE THE BELOW CHECK IN THE FUTURE!
     if response.status_code == 200 and not DANK_TOOL_VERSION in ("2.3.1", "2.3.2", "2.4") and f"v{DANK_TOOL_VERSION}" in (release["tag_name"] for release in response.json()):
 
         tmp = []
@@ -22,10 +22,9 @@ try:
             tmp.append(f"\n{release['tag_name']}\n{release['body']}")
         
         if tmp:
-            print(clr(f"\n  > Release Notes:"))
+            print(clr(f"\n  [ Release Notes ]"))
             for _ in tmp:
                 print(clr(_, colour_two=green))
-        
 except: pass
 
 # change directory
@@ -37,22 +36,22 @@ except:
 
 # download update
 
-print(clr("\n  > Downloading dank.tool.zip..."))
+print(clr("\n  - Downloading dank.tool.zip..."))
 while True:
     try: data = session.get("https://github.com/SirDank/dank.tool/raw/main/dank.tool.zip", allow_redirects=True).content; break
     except: input(clr("\n  > Failed to download! Make sure you are connected to the internet! Press [ENTER] to try again... ",2))
 
 try: open("dank.tool.zip","wb").write(data); del data
 except:
-    cls(); input(clr("\n  > Failed to save file!\n  > Please manually download the latest version from https://github.com/SirDank/dank.tool/raw/main/dank.tool.zip\n  > Press [ENTER] to EXIT... ",2))
+    cls(); input(clr("\n  - Failed to save file!\n  - Please manually download the latest version from https://github.com/SirDank/dank.tool/raw/main/dank.tool.zip\n  > Press [ENTER] to EXIT... ",2))
     sys.exit("Failed to save file!")
 
 # extract and execute installer
 
-print(clr("\n  > Extracting..."))
+print(clr("\n  - Extracting..."))
 try: pyminizip.uncompress("dank.tool.zip", "dankware", None, True)
 except: 
-    cls(); input(clr(f"\n  > Failed to extract!\n  > Please manually extract and install from \"{os.path.join(os.getcwd(), 'dank.tool.zip')}\" using the password \"dankware\"\n  > Press [ENTER] to EXIT... ",2))
+    cls(); input(clr(f"\n  - Failed to extract!\n  - Please manually extract and install from \"{os.path.join(os.getcwd(), 'dank.tool.zip')}\" using the password \"dankware\"\n  > Press [ENTER] to EXIT... ",2))
     sys.exit("Failed to extract file!")
     
 try: os.remove("password = dankware")
