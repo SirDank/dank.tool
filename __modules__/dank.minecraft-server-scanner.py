72962626f4c8ef2350b53a6317982f71901c8e9f
SirDank <52797753+SirDank@users.noreply.github.com>
2024-03-29 13:33:43 +0530

    .

--- __modules__/dank.minecraft-server-scanner.py
+++ __modules__/dank.minecraft-server-scanner.py
@@ -27,7 +27,7 @@ https://github.com/Footsiefat/Minecraft-Server-Scanner
 
 def translate(text):
     if DANK_TOOL_LANG:
-        try: text = translator.translate(text, source_language='en', destination_language=DANK_TOOL_LANG).result
+        try: text = translator.translate(text, DANK_TOOL_LANG, 'en').result
         except: pass
     return text
 
