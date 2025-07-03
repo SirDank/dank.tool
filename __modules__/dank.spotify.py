9a34367f8b276bfa8218b38794b715ad7a053783
SirDank <52797753+SirDank@users.noreply.github.com>
2025-07-03 20:50:10 +0530

    block special case error reporting
    - formatted code

--- __modules__/dank.spotify.py
+++ __modules__/dank.spotify.py
@@ -5,6 +5,8 @@ from rich.align import Align
 from dankware import cls, clr
 from rich.console import Console
 from translatepy import Translator
+from subprocess import CalledProcessError
+
 
 def run_command(command_list, check=True, capture=False, suppress_output=False):
     try:
@@ -17,39 +19,44 @@ def run_command(command_list, check=True, capture=False, suppress_output=False):
             text=True,
             stdout=stdout_pipe,
             stderr=stderr_pipe,
-            capture_output=capture
+            capture_output=capture,
         )
     except FileNotFoundError as exc:
-        raise FileNotFoundError(f"Command not found: '{command_list[0]}'. Make sure it's in your system PATH.") from exc
+        raise FileNotFoundError(
+            f"Command not found: '{command_list[0]}'. Make sure it's in your system PATH."
+        ) from exc
     # except subprocess.CalledProcessError as exc:
     #     raise RuntimeError(f"The following command failed: {command_list[0]}") from exc
     # except Exception as exc:
     #     raise RuntimeError(f"An unexpected error occurred running command: {exc}") from exc
 
+
 def translate(text):
     if DANK_TOOL_LANG:
-        try: text = translator.translate(text, DANK_TOOL_LANG, 'en').result
-        except: pass
+        try:
+            text = translator.translate(text, DANK_TOOL_LANG, "en").result
+        except:
+            pass
     return text
 
-def main():
 
+def main():
     global DANK_TOOL_LANG, translator
 
     # check if translator is enabled (dank.tool.exe)
 
     try:
-        DANK_TOOL_LANG = os.environ['DANK_TOOL_LANG']
-        if DANK_TOOL_LANG == 'en':
-            DANK_TOOL_LANG = ''
+        DANK_TOOL_LANG = os.environ["DANK_TOOL_LANG"]
+        if DANK_TOOL_LANG == "en":
+            DANK_TOOL_LANG = ""
         else:
             translator = Translator()
     except:
-        DANK_TOOL_LANG = ''
+        DANK_TOOL_LANG = ""
 
     # banner
 
-    banner1 = '\n\n .oooooo..o          amd64fox\'s      .   ooooooo  ooooo \nd8P\'    `Y8                        .o8    `8888    d8\'  \nY88bo.      oo.ooooo.   .ooooo.  .o888oo    Y888..8P    \n `"Y8888o.   888\' `88b d88\' `88b   888       `8888\'     \n     `"Y88b  888   888 888   888   888      .8PY888.    \noo     .d8P  888   888 888   888   888 .   d8\'  `888b   \n8""88888P\'   888bod8P\' `Y8bod8P\'   "888" o888o  o88888o \n             888                                        \n            o888o                                       \n'
+    banner1 = "\n\n .oooooo..o          amd64fox's      .   ooooooo  ooooo \nd8P'    `Y8                        .o8    `8888    d8'  \nY88bo.      oo.ooooo.   .ooooo.  .o888oo    Y888..8P    \n `\"Y8888o.   888' `88b d88' `88b   888       `8888'     \n     `\"Y88b  888   888 888   888   888      .8PY888.    \noo     .d8P  888   888 888   888   888 .   d8'  `888b   \n8\"\"88888P'   888bod8P' `Y8bod8P'   \"888\" o888o  o88888o \n             888                                        \n            o888o                                       \n"
     banner2 = '\nMP""""""`MM          oo                     dP   oo .8888b          \nM  mmmmm..M                                 88      88   "          \nM.      `YM 88d888b. dP .d8888b. .d8888b. d8888P dP 88aaa  dP    dP \nMMMMMMM.  M 88\'  `88 88 88\'  `"" 88ooood8   88   88 88     88    88 \nM. .MMM\'  M 88.  .88 88 88.  ... 88.  ...   88   88 88     88.  .88 \nMb.     .dM 88Y888P\' dP `88888P\' `88888P\'   dP   dP dP     `8888P88 \nMMMMMMMMMMM 88                                                  .88 \n            dP                                              d8888P  \n\n\n'
 
     cls()
@@ -64,34 +71,77 @@ def main():
 
     cls()
     print(clr(f"\n  - {translate('terminating Spotify...')}\n"))
-    run_command(['taskkill', '/f', '/t', '/im', 'spotify.exe'], check=False, suppress_output=True)
+    run_command(
+        ["taskkill", "/f", "/t", "/im", "spotify.exe"],
+        check=False,
+        suppress_output=True,
+    )
 
     # Spicetify restore
 
     print(clr(f"\n  - {translate('restoring Spotify...')}\n"))
-    run_command(['runas', '/trustlevel:0x20000', 'spicetify restore'], check=False)
+    run_command(["runas", "/trustlevel:0x20000", "spicetify restore"], check=False)
 
     # SpotX
 
-    translated = translate('installing SpotX...\n\n  [ RECOMMENDED SETTINGS ]\n  - Install Over\n  - Disable Podcasts\n  - Block Updates')
+    translated = translate(
+        "installing SpotX...\n\n  [ RECOMMENDED SETTINGS ]\n  - Install Over\n  - Disable Podcasts\n  - Block Updates"
+    )
     print(clr(f"\n  - {translated}"))
-    run_command(['powershell', '-NoProfile', '-ExecutionPolicy', 'Bypass', '-Command', 'iex "& { $(iwr -useb \'https://raw.githubusercontent.com/SpotX-Official/spotx-official.github.io/main/run.ps1\') } -new_theme"'], check=True)
+    run_command(
+        [
+            "powershell",
+            "-NoProfile",
+            "-ExecutionPolicy",
+            "Bypass",
+            "-Command",
+            "iex \"& { $(iwr -useb 'https://raw.githubusercontent.com/SpotX-Official/spotx-official.github.io/main/run.ps1') } -new_theme\"",
+        ],
+        check=True,
+    )
 
     input(clr(f"  > {translate('Hit [ ENTER ] if you are signed in to Spotify...')} "))
 
     print(clr(f"\n  - {translate('terminating Spotify...')}\n"))
-    run_command(['taskkill', '/f', '/t', '/im', 'spotify.exe'], check=False, suppress_output=True)
+    run_command(
+        ["taskkill", "/f", "/t", "/im", "spotify.exe"],
+        check=False,
+        suppress_output=True,
+    )
 
     # Spicetify
 
     print(clr(f"\n  - {translate('installing Spicetify-CLI...')}\n"))
-    run_command(['runas', '/trustlevel:0x20000', 'powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/spicetify/cli/main/install.ps1 | iex"'], check=True)
-    input(clr(f"  > {translate('Hit [ ENTER ] only after the new window has closed...')} "))
+    run_command(
+        [
+            "runas",
+            "/trustlevel:0x20000",
+            'powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/spicetify/cli/main/install.ps1 | iex"',
+        ],
+        check=True,
+    )
+    input(
+        clr(
+            f"  > {translate('Hit [ ENTER ] only after the new window has closed...')} "
+        )
+    )
 
-    translated = translate("[ SUGGESTED EXTENSIONS / THEMES ]\n  - Extension: Beautiful Lyrics\n  - Theme: Bloom (darkmono)")
+    translated = translate(
+        "[ SUGGESTED EXTENSIONS / THEMES ]\n  - Extension: Beautiful Lyrics\n  - Theme: Bloom (darkmono)"
+    )
     print(clr(f"\n  {translated}"))
 
     print(clr(f"\n  - {translate('Sleeping for 5 seconds...')}\n"))
     time.sleep(5)
 
-main()
+
+try:
+    main()
+except CalledProcessError:
+    print(
+        clr(
+            f"\n  - {translate('Known error occurred! Your system is unique, I need you to join my discord server and help me fix this error!')} ",
+            2,
+        )
+    )
+    input(clr(f"  > {translate('Hit [ ENTER ] to go back to the main menu')}... "))
--- __modules__/dank.winget.py
+++ __modules__/dank.winget.py
@@ -8,164 +8,272 @@ from rich.columns import Columns
 from rich.console import Console
 from dankware import cls, clr, rm_line, github_file_selector, green_bright
 
+
 def winget_installed():
     try:
-        result = subprocess.run(['winget', '--info'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=False)
+        result = subprocess.run(
+            ["winget", "--info"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            check=False,
+        )
         return bool(result.returncode == 0)
     except FileNotFoundError:
         return False
 
+
 def install_winget():
-    file_name = 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
-    url = github_file_selector('microsoft/winget-cli', 'add', ['.msixbundle'])[0]
+    file_name = "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+    url = github_file_selector("microsoft/winget-cli", "add", [".msixbundle"])[0]
     print(clr("\n  - Downloading..."))
-    data = requests.get(url, headers={'User-Agent': ('dank.tool' if "DANK_TOOL_VERSION" not in os.environ else f'dank.tool {os.environ["DANK_TOOL_VERSION"]}'), 'Content-Type': 'application/json'}, timeout=60, allow_redirects=True).content
+    data = requests.get(
+        url,
+        headers={
+            "User-Agent": (
+                "dank.tool"
+                if "DANK_TOOL_VERSION" not in os.environ
+                else f"dank.tool {os.environ['DANK_TOOL_VERSION']}"
+            ),
+            "Content-Type": "application/json",
+        },
+        timeout=60,
+        allow_redirects=True,
+    ).content
     print(clr("\n  - Downloaded!"))
     os.chdir(tempfile.gettempdir())
     with open(file_name, "wb") as file:
         file.write(data)
     os.system(file_name)
+    input(clr("\n  - Hit [ ENTER ] after you have installed winget... "))
+
 
 def print_banner():
     cls()
     banner = "\n\n   _         _           _             _   \n _| |___ ___| |_   _ _ _|_|___ ___ ___| |_ \n| . | .'|   | '_|_| | | | |   | . | -_|  _|\n|___|__,|_|_|_,_|_|_____|_|_|_|_  |___|_|  \n                              |___|        \n\n\n"
     Console().print(Align.center(banner), style="blink red", highlight=False)
-    print(clr("  [ Commands ]\n\n  - search <NAME OF SOFTWARE>\n\n  - installed (list of installed software)\n\n  - updates\n\n  - update-all\n\n  - clear (refresh screen)\n\n  - exit\n"))
+    print(
+        clr(
+            "  [ Commands ]\n\n  - search <NAME OF SOFTWARE>\n\n  - installed (list of installed software)\n\n  - updates\n\n  - update-all\n\n  - clear (refresh screen)\n\n  - exit\n"
+        )
+    )
+
 
 def cleanup_result(cmd):
     for i, line in enumerate(cmd):
-        cmd[i] = line.strip().replace('  ', '')
+        cmd[i] = line.strip().replace("  ", "")
     return cmd
 
-def handle_response(cmd, results, mode):
 
+def handle_response(cmd, results, mode):
     indexes = [0]
-    cmd = cmd.stdout.decode('utf-8').splitlines()
-    if not (_ for _ in cmd if _.startswith('Name')):
-        raise RuntimeError(f"Error parsing response\n  - mode: {mode}\n  - cmd:\n\n{cleanup_result(cmd)}")
+    cmd = cmd.stdout.decode("utf-8").splitlines()
+    if not (_ for _ in cmd if _.startswith("Name")):
+        raise RuntimeError(
+            f"Error parsing response\n  - mode: {mode}\n  - cmd:\n\n{cleanup_result(cmd)}"
+        )
 
     try:
-        while not cmd[0].startswith('Name'):
+        while not cmd[0].startswith("Name"):
             cmd = cmd[1:]
     except IndexError as exc:
-        raise RuntimeError(f"Error parsing response\n  - mode: {mode}\n  - cmd:\n\n{cleanup_result(cmd)}") from exc
+        raise RuntimeError(
+            f"Error parsing response\n  - mode: {mode}\n  - cmd:\n\n{cleanup_result(cmd)}"
+        ) from exc
 
     try:
-        for char in ('I', 'V', 'M', 'S'):
+        for char in ("I", "V", "M", "S"):
             if char in cmd[0]:
                 indexes.append(cmd[0].index(char))
     except Exception as exc:
-        raise RuntimeError(f"Error parsing response\n  - mode: {mode}\n  - cmd:\n\n{cleanup_result(cmd)}") from exc
+        raise RuntimeError(
+            f"Error parsing response\n  - mode: {mode}\n  - cmd:\n\n{cleanup_result(cmd)}"
+        ) from exc
 
     results.clear()
     cmd = cmd[2:]
     index = 1
 
     for line in cmd:
-        parts = [line[i:j] for i,j in zip(indexes, indexes[1:]+[None])]
-        if '.' in parts[1].strip():
-            results[index] = {'name': parts[0].strip(), 'id': parts[1].strip()} #, 'version': parts[2].strip(), 'source': parts[3].strip()}
+        parts = [line[i:j] for i, j in zip(indexes, indexes[1:] + [None])]
+        if "." in parts[1].strip():
+            results[index] = {
+                "name": parts[0].strip(),
+                "id": parts[1].strip(),
+            }  # , 'version': parts[2].strip(), 'source': parts[3].strip()}
             index += 1
 
     console = Console()
-    user_renderables = [f"[b][bright_white]{key} [bright_red]- [bright_white]{value['name']}[/b]" for key, value in results.items()]
-    results['mode'] = mode; print()
-    console.print(Panel(title="[red1]> [bright_white][b]R E S U L T S[/b] [red1]<", title_align="center", renderable=Columns(user_renderables, expand=True), style="red", expand=True))
+    user_renderables = [
+        f"[b][bright_white]{key} [bright_red]- [bright_white]{value['name']}[/b]"
+        for key, value in results.items()
+    ]
+    results["mode"] = mode
+    print()
+    console.print(
+        Panel(
+            title="[red1]> [bright_white][b]R E S U L T S[/b] [red1]<",
+            title_align="center",
+            renderable=Columns(user_renderables, expand=True),
+            style="red",
+            expand=True,
+        )
+    )
 
     match mode:
-        case 'search':
+        case "search":
             print(clr("\n  - Type number to install ( Supports multiple ex: 1,2,3 )\n"))
-        case 'installed':
-            print(clr("\n  - Type number to display info ( Supports multiple ex: 1,2,3 )\n"))
-        case 'updates':
+        case "installed":
+            print(
+                clr(
+                    "\n  - Type number to display info ( Supports multiple ex: 1,2,3 )\n"
+                )
+            )
+        case "updates":
             print(clr("\n  - Type number to update ( Supports multiple ex: 1,2,3 )\n"))
 
+
 def print_info(id):
-    cmd = subprocess.run(['winget', 'show', '--id', id], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=False)
+    cmd = subprocess.run(
+        ["winget", "show", "--id", id],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        check=False,
+    )
     if cmd.returncode == 0:
-        print(clr(cmd.stdout.decode('utf-8')))
+        print(clr(cmd.stdout.decode("utf-8")))
     else:
-        print(clr(f"\n  [ERROR]: {cmd.stdout.decode('utf-8')}",2))
+        print(clr(f"\n  [ERROR]: {cmd.stdout.decode('utf-8')}", 2))
+
 
 def main():
-
     results = {}
     print_banner()
 
     while True:
+        cmd = input(clr("  > ") + green_bright)
 
-        cmd = input(clr('  > ') + green_bright)
-
-        if cmd.lower().startswith('search '):
-            cmd = subprocess.run(['winget', 'search', '--accept-source-agreements', cmd[7:]], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=False)
+        if cmd.lower().startswith("search "):
+            cmd = subprocess.run(
+                ["winget", "search", "--accept-source-agreements", cmd[7:]],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                check=False,
+            )
             if cmd.returncode == 0:
-                if len(cmd.stdout.decode('utf-8').splitlines()) > 0:
-                    handle_response(cmd, results, 'search')
+                if len(cmd.stdout.decode("utf-8").splitlines()) > 0:
+                    handle_response(cmd, results, "search")
                 else:
-                    print(clr("\n  [KNOWN ERROR]: Please report this issue on GitHub / Discord Support Ticket to help fix it!",2))
+                    print(
+                        clr(
+                            "\n  [KNOWN ERROR]: Please report this issue on GitHub / Discord Support Ticket to help fix it!",
+                            2,
+                        )
+                    )
             else:
-                print(clr(f"\n  [ERROR]: {cmd.stdout.decode('utf-8')}",2))
+                print(clr(f"\n  [ERROR]: {cmd.stdout.decode('utf-8')}", 2))
 
-        elif cmd.lower().startswith('installed'):
-            cmd = subprocess.run(['winget', 'list', '--accept-source-agreements'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=False)
+        elif cmd.lower().startswith("installed"):
+            cmd = subprocess.run(
+                ["winget", "list", "--accept-source-agreements"],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                check=False,
+            )
             if cmd.returncode == 0:
-                if len(cmd.stdout.decode('utf-8').splitlines()) > 0:
-                    handle_response(cmd, results, 'installed')
+                if len(cmd.stdout.decode("utf-8").splitlines()) > 0:
+                    handle_response(cmd, results, "installed")
                 else:
-                    print(clr("\n  [KNOWN ERROR]: Please report this issue on GitHub / Discord Support Ticket to help fix it!",2))
+                    print(
+                        clr(
+                            "\n  [KNOWN ERROR]: Please report this issue on GitHub / Discord Support Ticket to help fix it!",
+                            2,
+                        )
+                    )
             else:
-                print(clr(f"\n  [ERROR]: {cmd.stdout.decode('utf-8')}",2))
+                print(clr(f"\n  [ERROR]: {cmd.stdout.decode('utf-8')}", 2))
 
-        elif cmd.lower().startswith('updates') or cmd.lower().startswith('update-all'):
+        elif cmd.lower().startswith("updates") or cmd.lower().startswith("update-all"):
             choice = cmd.lower()
-            cmd = subprocess.run(['winget', 'upgrade', '--accept-source-agreements'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=False)
+            cmd = subprocess.run(
+                ["winget", "upgrade", "--accept-source-agreements"],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                check=False,
+            )
             if cmd.returncode == 0:
-                if len(cmd.stdout.decode('utf-8').splitlines()) > 0:
-                    if not choice.startswith('update-all'):
-                        handle_response(cmd, results, 'updates')
+                if len(cmd.stdout.decode("utf-8").splitlines()) > 0:
+                    if not choice.startswith("update-all"):
+                        handle_response(cmd, results, "updates")
                     else:
-                        handle_response(cmd, results, 'update-all')
+                        handle_response(cmd, results, "update-all")
                         max = len(results) - 1
                         for index in range(1, max + 1):
-                            print(clr(f"\n  - [{index}/{max}] Updating {results[index]['name']}...\n"))
-                            os.system(f"winget upgrade --interactive --id {results[index]['id']}")
+                            print(
+                                clr(
+                                    f"\n  - [{index}/{max}] Updating {results[index]['name']}...\n"
+                                )
+                            )
+                            os.system(
+                                f"winget upgrade --interactive --id {results[index]['id']}"
+                            )
                         print()
                 else:
-                    print(clr("\n  [KNOWN ERROR]: Please report this issue on GitHub / Discord Support Ticket to help fix it!",2))
+                    print(
+                        clr(
+                            "\n  [KNOWN ERROR]: Please report this issue on GitHub / Discord Support Ticket to help fix it!",
+                            2,
+                        )
+                    )
             else:
-                print(clr(f"\n  [ERROR]: {cmd.stdout.decode('utf-8')}",2))
+                print(clr(f"\n  [ERROR]: {cmd.stdout.decode('utf-8')}", 2))
 
-        elif cmd.lower().startswith('clear'):
+        elif cmd.lower().startswith("clear"):
             print_banner()
 
-        elif cmd.lower().startswith('exit'):
+        elif cmd.lower().startswith("exit"):
             break
 
-        elif all(_.isdigit() for _ in cmd.strip().split(',')):
-
+        elif all(_.isdigit() for _ in cmd.strip().split(",")):
             if results:
-
-                for selected in cmd.split(','):
+                for selected in cmd.split(","):
                     selected = int(selected)
                     if selected in results:
-
-                        match results['mode']:
-                            case 'search':
-                                if input(clr("\n  > Display info? [y/n]: ") + green_bright).lower().startswith('y'):
-                                    print(); print_info(results[selected]['id'])
-                                else: rm_line(); rm_line()
-                                if input(clr("\n  > Install? [y/n]: ") + green_bright).lower().startswith('y'):
+                        match results["mode"]:
+                            case "search":
+                                if (
+                                    input(
+                                        clr("\n  > Display info? [y/n]: ")
+                                        + green_bright
+                                    )
+                                    .lower()
+                                    .startswith("y")
+                                ):
                                     print()
-                                    os.system(f"winget install --interactive --id {results[selected]['id']}")
+                                    print_info(results[selected]["id"])
+                                else:
+                                    rm_line()
+                                    rm_line()
+                                if (
+                                    input(clr("\n  > Install? [y/n]: ") + green_bright)
+                                    .lower()
+                                    .startswith("y")
+                                ):
                                     print()
-                                else: rm_line()
+                                    os.system(
+                                        f"winget install --interactive --id {results[selected]['id']}"
+                                    )
+                                    print()
+                                else:
+                                    rm_line()
 
-                            case 'installed':
-                                print_info(results[selected]['id'])
+                            case "installed":
+                                print_info(results[selected]["id"])
 
-                            case 'updates':
+                            case "updates":
                                 print()
-                                os.system(f"winget upgrade --interactive --id {results[selected]['id']}")
+                                os.system(
+                                    f"winget upgrade --interactive --id {results[selected]['id']}"
+                                )
                                 print()
 
                             case _:
@@ -177,11 +285,30 @@ def main():
         else:
             rm_line()
 
-if os.name != 'nt' or 'WINELOADER' in os.environ:
-    input(clr("\n  - This module only works for Windows! Press [ ENTER ] to exit... ",2))
-elif not winget_installed():
-    input(clr("\n  - This module requires winget to be installed! Press [ ENTER ] to download and install... "))
-    install_winget()
-    main()
-else:
-    main()
+
+try:
+    if os.name != "nt" or "WINELOADER" in os.environ:
+        input(
+            clr(
+                "\n  - This module only works for Windows! Press [ ENTER ] to exit... ",
+                2,
+            )
+        )
+    elif not winget_installed():
+        input(
+            clr(
+                "\n  - This module requires winget to be installed! Press [ ENTER ] to download and install... "
+            )
+        )
+        install_winget()
+        main()
+    else:
+        main()
+except RuntimeError:
+    print(
+        clr(
+            "\n  - Known error occurred! Your system is unique, I need you to join my discord server and help me fix this error!",
+            2,
+        )
+    )
+    input(clr("  > Hit [ ENTER ] to go back to the main menu... "))
